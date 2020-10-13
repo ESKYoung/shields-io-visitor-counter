8f633f9f05a93c6ac27d9d6e7133e2ec459ef21e
ESKYoung <ESKYoung@users.noreply.github.com>
2020-10-13 21:23:48 +0100

    Add get_page_hash function, and associated test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, redirect
+import hashlib
 import os
 import werkzeug
 
 # Import environmental variables
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
+HASH_KEY = os.getenv("HASH_KEY")
 
 # Initialise the flask app
 app = Flask(__name__)
@@ -18,6 +20,28 @@ def redirect_to_github_repository() -> werkzeug.wrappers.Response:
 
     """
     return redirect(GITHUB_REPOSITORY, code=302)
+
+
+def get_page_hash(page: str) -> str:
+    """Get a SHA3-512 hashed version of page.
+
+    Args:
+        page: A string giving the name of the page.
+
+    Returns:
+        A SHA3-512 hashed version of the string combined with the HASH_KEY environmental variable.
+
+    """
+
+    # Instantiate a SHA3-512 object
+    obj_hash = hashlib.sha3_512()
+
+    # Hash the page, and update with HASH_KEY
+    obj_hash.update(page.encode("utf-8"))
+    obj_hash.update(HASH_KEY.encode("utf-8"))
+
+    # Return the hashed key
+    return obj_hash.hexdigest()
 
 
 if __name__ == '__main__':
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
-from main import app, redirect_to_github_repository
+from main import app, get_page_hash, redirect_to_github_repository
 import os
+import pytest
 
 # Import environmental variables
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
@@ -23,4 +24,24 @@ class TestRedirectToGithubRepository:
         _ = app.test_client().get("/")
 
         # Execute the redirect_to_github_repository function
-        assert patch_flask_redirect() == redirect_to_github_repository()
+        assert redirect_to_github_repository() == patch_flask_redirect()
+
+
+# Define test cases for the test_get_page_hash_returns_correctly test
+args_test_get_page_hash_returns_correctly = [
+    ("foo", "bar", "ff32a30c3af5012ea395827a3e99a13073c3a8d8410a708568ff7e6eb85968fccfebaea039bc21411e9d43fdb9a851b529b"
+                   "9960ffea8679199781b8f45ca85e2"),
+    ("bar", "foo", "b491a039c51aab2e18c4a7f38401981a078730408bd939df651668cecaf10c1145c55688b28b9f96fd9b966daf66a945131"
+                   "aa59c3fed7f321f3fdfc3c47c5b9c")
+]
+
+
+@pytest.mark.parametrize("test_input_page, test_input_hash, test_expected", args_test_get_page_hash_returns_correctly)
+def test_get_page_hash_returns_correctly(mocker, test_input_page: str, test_input_hash: str,
+                                         test_expected: str) -> None:
+    """Test get_page_hash returns the correct value."""
+
+    # Patch the HASH_KEY environment variable
+    _ = mocker.patch("main.HASH_KEY", test_input_hash)
+
+    assert get_page_hash(test_input_page) == test_expected
